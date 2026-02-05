312ac36516e6608788444e380183f58d4c9efde0
mafaffi77 <ethiomiracle2017@gmail.com>
2026-02-05 19:43:25 +0300

    update Power - Mod Power

--- HackerRank/Hacker-Rank-Power-Mod Power/Power - Mod Power.py
+++ HackerRank/Hacker-Rank-Power-Mod Power/Power - Mod Power.py
@@ -1,14 +1,8 @@
-if __name__ == '__main__':
-    
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-    result= [[i,j,k] for k in range(z) for j in range(y) for i in range(x)]
-    newnew=[]
-    print(result)
-    for num in result:
-        if sum(num)<n:
-            newnew.append(num)
-    print(newnew)
-        
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a, b))
+print(pow(a, b, m))
