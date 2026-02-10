90f25d08008d0dd933b8bbb5720af5414a04e07d
mafaffi77 <ethiomiracle2017@gmail.com>
2026-02-10 22:29:24 +0300

    Frequency Tracker

---
+++ Leetcode/2671-Frequency-Tracker/2671-Frequency-Tracker.py
@@ -0,0 +1,42 @@
+# Frequency Tracker
+# Platform: LeetCode
+
+from collections import defaultdict
+
+class FrequencyTracker:
+    def __init__(self):
+        self.count = defaultdict(int)   
+        self.freq = defaultdict(int)    
+
+    def add(self, number: int) -> None:
+        d = self.count[number]
+        if d > 0:
+            self.freq[d] -= 1
+
+        self.count[number] += 1
+        self.freq[d + 1] += 1
+
+    def deleteOne(self, number: int) -> None:
+        if self.count[number] == 0:
+            return
+
+        d = self.count[number]
+        self.freq[d] -= 1
+        self.count[number] -= 1
+
+        if self.count[number] > 0:
+            self.freq[d - 1] += 1
+        else:
+            del self.count[number]
+
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.freq[frequency] > 0
+
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
+
--- create_problem.py
+++ create_problem.py
@@ -12,33 +12,45 @@ def create_files(base, folder, filename, title, platform):
     solution_template = f"""# {title}
 # Platform: {platform}
 
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        value=
-        total=0
-        pre=0
-        for char in reversed(s):
-            val= value[char]
-            if val<pre:
-                total-=val
-            else:
-                total+=val
-            pre=val
-        return total
-## or 
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        value= 
-        total=0
-        pre=0
-        for char in reversed(s):
-            val= value[char]
-            if val<pre:
-                total-=val
-            else:
-                total+=val
-            pre=val
-        return total
+from collections import defaultdict
+
+class FrequencyTracker:
+    def __init__(self):
+        self.count = defaultdict(int)   
+        self.freq = defaultdict(int)    
+
+    def add(self, number: int) -> None:
+        d = self.count[number]
+        if d > 0:
+            self.freq[d] -= 1
+
+        self.count[number] += 1
+        self.freq[d + 1] += 1
+
+    def deleteOne(self, number: int) -> None:
+        if self.count[number] == 0:
+            return
+
+        d = self.count[number]
+        self.freq[d] -= 1
+        self.count[number] -= 1
+
+        if self.count[number] > 0:
+            self.freq[d - 1] += 1
+        else:
+            del self.count[number]
+
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.freq[frequency] > 0
+
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
+
 """
 
     # ---- README template ----
@@ -47,51 +59,67 @@ class Solution:
 ## Platform
 {platform}
 
-## 13. Roman to Integer
+## 2671. Frequency Tracker
 
-Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+Design a data structure that keeps track of the values in it and answers some queries regarding their frequencies.
 
-Symbol       Value
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
-For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+Implement the FrequencyTracker class.
 
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-I can be placed before V (5) and X (10) to make 4 and 9. 
-X can be placed before L (50) and C (100) to make 40 and 90. 
-C can be placed before D (500) and M (1000) to make 400 and 900.
-Given a roman numeral, convert it to an integer.
-
+FrequencyTracker(): Initializes the FrequencyTracker object with an empty array initially.
+void add(int number): Adds number to the data structure.
+void deleteOne(int number): Deletes one occurrence of number from the data structure. The data structure may not contain number, and in this case nothing is deleted.
+bool hasFrequency(int frequency): Returns true if there is a number in the data structure that occurs frequency number of times, otherwise, it returns false.
  
 
 Example 1:
 
-Input: s = "III"
-Output: 3
-Explanation: III = 3.
+Input
+["FrequencyTracker", "add", "add", "hasFrequency"]
+[[], [3], [3], [2]]
+Output
+[null, null, null, true]
+
+Explanation
+FrequencyTracker frequencyTracker = new FrequencyTracker();
+frequencyTracker.add(3); // The data structure now contains [3]
+frequencyTracker.add(3); // The data structure now contains [3, 3]
+frequencyTracker.hasFrequency(2); // Returns true, because 3 occurs twice
+
 Example 2:
 
-Input: s = "LVIII"
-Output: 58
-Explanation: L = 50, V= 5, III = 3.
+Input
+["FrequencyTracker", "add", "deleteOne", "hasFrequency"]
+[[], [1], [1], [1]]
+Output
+[null, null, null, false]
+
+Explanation
+FrequencyTracker frequencyTracker = new FrequencyTracker();
+frequencyTracker.add(1); // The data structure now contains [1]
+frequencyTracker.deleteOne(1); // The data structure becomes empty []
+frequencyTracker.hasFrequency(1); // Returns false, because the data structure is empty
+
 Example 3:
 
-Input: s = "MCMXCIV"
-Output: 1994
-Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+Input
+["FrequencyTracker", "hasFrequency", "add", "hasFrequency"]
+[[], [2], [3], [1]]
+Output
+[null, false, null, true]
+
+Explanation
+FrequencyTracker frequencyTracker = new FrequencyTracker();
+frequencyTracker.hasFrequency(2); // Returns false, because the data structure is empty
+frequencyTracker.add(3); // The data structure now contains [3]
+frequencyTracker.hasFrequency(1); // Returns true, because 3 occurs once
+
  
 
 Constraints:
 
-1 <= s.length <= 15
-s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
-It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+1 <= number <= 105
+1 <= frequency <= 105
+At most, 2 * 105 calls will be made to add, deleteOne, and hasFrequency in total.
 
 """
 
