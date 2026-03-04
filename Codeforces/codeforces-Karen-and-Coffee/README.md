82b6da51ba36204f3c8e697e2a1dc3b4b26839c3
mafaffi77 <ethiomiracle2017@gmail.com>
2026-03-04 21:32:09 +0300

    add Karen and Coffee

---
+++ Codeforces/codeforces-Karen-and-Coffee/README.md
@@ -0,0 +1,7 @@
+# Karen and Coffee
+
+## Platform
+Codeforces
+## B. Karen and Coffee
+
+## problem link:https://codeforces.com/contest/816/problem/B
--- create_problem.py
+++ create_problem.py
@@ -11,31 +11,24 @@ def create_files(base, folder, filename, title, platform):
     # ---- Solution template ----
     solution_template = f"""# {title}
 # Platform: {platform}
-import sys
-input = sys.stdin.readline
-from collections import defaultdict
- 
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
- 
-freq = defaultdict(int)
-left, unique, best_length, best_left, best_right = 0,0,0,0,0
- 
-for right in range(n):
-    if freq[a[right]] == 0:
-        unique += 1
-    freq[a[right]] += 1
- 
-    while unique > k:
-        freq[a[left]] -= 1
-        if freq[a[left]] == 0:
-            unique -= 1
-        left += 1
-    if right - left + 1 > best_length:  #current window is valid
-        best_length = right - left + 1
-        best_left = left
-        best_right = right
-print(best_left + 1, best_right + 1)  #1-based idx
+a , b , c = list(map(int, input().split()))
+r= 200005
+arr = [0] *r
+for _ in range(a):
+    x , y = list(map(int , input().split()))
+    arr[x] += 1
+    arr[y + 1]-= 1
+for i in range(1 , r):
+    arr[i] += arr[i - 1]
+f = [0] * r
+for t in range(r):
+    if arr[t] >= b :
+        f[t] += 1
+for t in range(1 , r):
+    f[t] +=f[t - 1]
+for g in range(c):
+    x, y = list(map(int , input().split()))
+    print(f[y] - f[x - 1])
     
 """
 
@@ -44,9 +37,9 @@ print(best_left + 1, best_right + 1)  #1-based idx
 
 ## Platform
 {platform}
-## D. Longest k-Good Segment
+## B. Karen and Coffee
 
-## problem link:https://codeforces.com/contest/616/problem/D
+## problem link:https://codeforces.com/contest/816/problem/B
 """
 
     # ---- NOTES template ----
