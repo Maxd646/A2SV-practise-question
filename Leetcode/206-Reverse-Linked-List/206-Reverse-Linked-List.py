fde47bfab46d1cee3586ccb64ae88d058575de16
mafaffi77 <ethiomiracle2017@gmail.com>
2026-03-05 20:36:45 +0300

    add Reverse Linked List

---
+++ Leetcode/206-Reverse-Linked-List/206-Reverse-Linked-List.py
@@ -0,0 +1,20 @@
+# Reverse Linked List
+# Platform: LeetCode
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p=head
+        q=None
+        r=None
+        while p:
+           r=q
+           q=p
+           p=p.next
+           q.next=r     
+
+        return q
+
--- create_problem.py
+++ create_problem.py
@@ -11,96 +11,23 @@ def create_files(base, folder, filename, title, platform):
     # ---- Solution template ----
     solution_template = f"""# {title}
 # Platform: {platform}
-class Node:
-    def __init__(self, val):
-        self.val=val
-        self.next=None
-class MyLinkedList:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p=head
+        q=None
+        r=None
+        while p:
+           r=q
+           q=p
+           p=p.next
+           q.next=r     
 
-    def __init__(self):
-        self.head=None
-        self.tail=None
-
-    def get(self, index: int) -> int:
-        if index<0:
-            return -1
-        curr=self.head
-        i=0
-        while i<index and curr:
-            curr=curr.next
-            i+=1
-        if curr is not None:
-            return curr.val
-        else:
-            return -1
-
-    def addAtHead(self, val: int) -> None:
-        temp=Node(val)
-        if self.head is None:
-            self.head=temp
-            self.tail=temp
-        else:
-            temp.next=self.head
-            self.head=temp
-        
-    def addAtTail(self, val: int) -> None:
-        temp=Node(val)
-        if self.head is None:
-            self.head=self.tail=temp
-        else:
-            self.tail.next=temp
-            self.tail=temp
-        
-    def addAtIndex(self, index: int, val: int) -> None:
-        temp=Node(val) 
-        if index == 0:
-            self.addAtHead(val)
-        elif self.head is None:
-            return 
-        else:
-            current=self.head
-            inde=0
-            while inde<index-1 and current:
-                current=current.next
-                inde+=1
-            if current and inde == index-1:
-                temp.next=current.next
-                current.next=temp
-                if temp.next is None:
-                    self.tail=temp
-    
-    def deleteAtIndex(self, index: int) -> None:
-        if index<0:
-            return 
-        elif self.head is None:
-            return 
-        elif index==0:
-            temp=self.head
-            self.head=self.head.next
-            if self.head is None:
-                self.tail=None
-        else:
-            current=self.head
-            ind=0
-            while ind<index-1 and current.next:
-                ind+=1
-                current=current.next
-            if current.next is None:
-                return
-
-            if current.next == self.tail:
-                self.tail = current
-
-            current.next = current.next.next
-               
-
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList()
-# param_1 = obj.get(index)
-# obj.addAtHead(val)
-# obj.addAtTail(val)
-# obj.addAtIndex(index,val)
-# obj.deleteAtIndex(index)
+        return q
 
 """
 
@@ -109,48 +36,38 @@ class MyLinkedList:
 
 ## Platform
 {platform}
-## 707. Design Linked List
+## 206. Reverse Linked List
 
-Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
-A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
-If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+Given the head of a singly linked list, reverse the list, and return the reversed list.
 
-Implement the MyLinkedList class:
-
-MyLinkedList() Initializes the MyLinkedList object.
-int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
-void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-void addAtTail(int val) Append a node of value val as the last element of the linked list.
-void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
-void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
  
 
 Example 1:
 
-Input
-["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
-[[], [1], [3], [1, 2], [1], [1], [1]]
-Output
-[null, null, null, null, 2, null, 3]
 
-Explanation
-MyLinkedList myLinkedList = new MyLinkedList();
-myLinkedList.addAtHead(1);
-myLinkedList.addAtTail(3);
-myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
-myLinkedList.get(1);              // return 2
-myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
-myLinkedList.get(1);              // return 3
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
  
 
 Constraints:
 
-0 <= index, val <= 1000
-Please do not use the built-in LinkedList library.
-At most 2000 calls will be made to get, addAtHead, addAtTail, addAtIndex and deleteAtIndex.
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 
 
-## problem link:https://leetcode.com/problems/design-linked-list/description/
+## problem link:https://leetcode.com/problems/reverse-linked-list/description/
 """
 
     # ---- NOTES template ----
