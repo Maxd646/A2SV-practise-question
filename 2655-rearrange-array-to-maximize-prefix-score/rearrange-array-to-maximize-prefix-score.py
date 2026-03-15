e4110d3d884dad3c15b51223f0fdfb6dbd782659
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-03-16 01:26:38 +0300

    Time: 111 ms (17.92%) | Memory: 33.1 MB (5.71%) - LeetSync

--- 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.py
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.py
@@ -1,16 +1,9 @@
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
-        nums.sort()
-        n=len(nums)
-        j=0
-        found=False
-        for i in range(n):
-            if nums[i]>0:
-                j=i
-                found=True
-                break
-
-        nums=list(accumulate(nums[j:]+nums[:j][::-1]))
+        n= len(nums)
+        nums.sort(reverse =True)
+        nums=list(accumulate(nums))
+        print(nums)
     
         for i in range(n):
             if nums[i]<=0:
