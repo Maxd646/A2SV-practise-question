c47e77b96677f12dc7ba8b67dfbe8f12476ccb26
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-03-21 19:36:59 +0300

    Time: 3 ms (47.56%) | Memory: 19.3 MB (69.91%) - LeetSync

---
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minDeletion(self, s: str, k: int) -> int:
+        seen= Counter(s)
+        if len(seen)<=k:
+            return 0
+        seen= sorted(seen.items(), key= lambda x: x[1], reverse=True)
+        print(seen[:k])
+        ans=0
+        for num, val in seen[k:]:
+            ans+=val
+        return ans
+
+
+
+        
+        
+        
+            
+        
+
+        
