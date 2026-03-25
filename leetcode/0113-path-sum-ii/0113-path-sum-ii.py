2d3e685dd2511b1a45655debd47ac5eb616c3d24
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-03-25 23:59:38 +0300

    Add solution for Path Sum II

---
+++ leetcode/0113-path-sum-ii/0113-path-sum-ii.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        ans=[]
+        stack=[]
+        def pre(root, tr):
+            if not root:
+                return False
+            stack.append(root.val)
+            if not root.left and not root.right:
+                if sum(stack)==tr:
+                    ans.append(stack[:])
+                    stack.pop()
+                    return 
+                stack.pop()
+                return False
+            if pre(root.left, tr):
+                return True 
+            if pre(root.right, tr):
+                return True
+            stack.pop()
+            return False
+        pre(root, targetSum )
+            
+        return ans
+        
+
+        
+
+        
