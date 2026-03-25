4a43e34e7aa54cd41faba452ed851e301e41cdb2
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-03-25 15:03:06 +0300

    Add solution for Construct Binary Tree from Preorder and Postorder Traversal

--- leetcode/0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ leetcode/0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -13,8 +13,6 @@ class Solution:
         if len(preorder) == 1:
             return root
         mid= postorder.index(preorder[1])
-        print(mid)
-        print(preorder, postorder)
         root.left = self. constructFromPrePost(preorder[1:mid+2], postorder[:mid+1])
         root.right = self. constructFromPrePost(preorder[mid+2:], postorder[mid+1:-1])
         return root
