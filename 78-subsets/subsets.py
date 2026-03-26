b67b25b6046eb2b3814baefcf77f63f9284f45e0
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-03-26 14:00:30 +0300

    Time: 2 ms (8.81%) | Memory: 19.4 MB (52.23%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,16 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
         def backtrack(st, poss, l):
             if len(poss) == l:
                 ans.append(poss[:])
                 return
+            if len(poss)>l:
+                return 
             for j in range(st, n):
                 poss.append(nums[j])
                 backtrack(j + 1, poss, l)
                 poss.pop()
-        ans = []
+        
         n= len(nums)
-        for i in range(n+ 1):
+        for i in range(n+1):
             backtrack(0, [], i)
         return ans
     
