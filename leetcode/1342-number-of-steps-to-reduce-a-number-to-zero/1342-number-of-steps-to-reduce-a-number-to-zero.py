427c336d1a6ee421faba09f64da1772091b8d1e7
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-03-28 17:12:31 +0300

    Add solution for Number of Steps to Reduce a Number to Zero

---
+++ leetcode/1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfSteps(self, n: int) -> int:
+        ans = 0
+        while n>0:
+            if n%2==0:
+                ans+=1
+                n/=2
+            else:
+                ans+=1
+                n-=1
+        return ans
+        
