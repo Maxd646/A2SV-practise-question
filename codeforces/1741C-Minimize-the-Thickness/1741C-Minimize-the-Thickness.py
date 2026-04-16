401947f9f2ab18c29bf09302a0c2576a7391341e
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-04-16 19:00:46 +0300

    Add solution for Minimize the Thickness

---
+++ codeforces/1741C-Minimize-the-Thickness/1741C-Minimize-the-Thickness.py
@@ -0,0 +1,56 @@
+import sys, math, heapq, itertools
+from itertools import permutations, combinations, combinations_with_replacement
+from itertools import product, accumulate, groupby, chain
+from collections import defaultdict, Counter, deque
+from bisect import bisect_left, bisect_right
+from functools import lru_cache
+
+input = sys.stdin.readline
+number = lambda: int(input())
+numbers = lambda: list(map(int, input().split()))
+words = lambda: sys.stdin.readline().strip().split()
+word = lambda: sys.stdin.readline().strip()
+minn = float('inf')
+maxx = float('-inf')
+MOD = 10**9 + 7
+
+YES = lambda: print('YES')
+NO = lambda: print('NO')
+
+#sys.setrecursionlimit(10**7)
+@lru_cache(None)
+def dp():
+    pass
+
+test_cases = lambda inp=0: number() if not inp else inp
+
+def solve():
+    n = number()
+    arr = numbers()
+    ans = float('inf')
+    summ = 0
+    for i in range(n):
+        summ += arr[i]
+        seen = [i+1]
+        found = True
+        summ1= 0
+        k=i
+        for j in range(i+1, n):
+            summ1+=arr[j]
+            if summ1==summ:
+                seen.append(j-k) 
+                k=j 
+                summ1=0 
+            elif summ1>summ:
+                found=False
+                break
+            elif j==n-1 and summ1<summ:
+                    found=False
+                    break
+        if  found:
+            ans = min(ans, max(seen))
+    print(ans)
+    return
+
+for _ in range(test_cases()):
+    solve()
