c4471aa19f6f56dd68694599b76d056b7ce4e307
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-04-22 00:19:04 +0300

    Add solution for Maximum Subarray With Equal Products

---
+++ leetcode/3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        maxx =0
+        def GCD(nums):
+            return reduce(gcd, nums)
+
+        def LCM(nums):
+            def rlcm(a, b):
+                return (a*b)//gcd(a, b)
+            return reduce(rlcm, nums)
+            
+        for i in range(n):
+            for j in range(i, n):
+                sub = nums[i:j+1]
+                g= GCD(sub)
+                l = LCM(sub)
+                if prod(sub) == g*l:
+                    maxx = max(maxx, j-i+1)
+        return maxx
+        
+
+            
+        
