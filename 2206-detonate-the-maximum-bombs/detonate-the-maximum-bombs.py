fdc241abf96a7dc6606fc67157904dd08f7eca10
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-04-24 19:46:36 +0300

    Time: 343 ms (45.94%) | Memory: 21.2 MB (31.90%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
@@ -1,28 +1,29 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         n = len(bombs)
-        graph = defaultdict(list)
+        graph =  [[] for _ in range(n)]
         for i in range(n):
             x, y, r = bombs[i]
-            for j in range( n):
-                xi, yi, _ = bombs[j]
-                if i == j:
+            for j in range(n):
+                if i== j:
                     continue
-                distance = math.sqrt((xi - x) ** 2 + (yi - y) ** 2)
-                if r >= distance:
+                xi, yi, _ = bombs[j]
+                distance = ((abs(x-xi)**2+abs(y-yi)**2))**0.5
+                if r>=distance:
                     graph[i].append(j)
         lru_cache(None)
         def dfs(node, visited):
+            nonlocal count
             visited.add(node)
-            count = 1
+            count+=1
             for child in graph[node]:
                 if child not in visited:
-                    count += dfs(child, visited)
-            return count
+                    dfs(child, visited)
         maxx = 0
         for i in range(n):
-            visited = set()
-            maxx = max(maxx, dfs(i, visited))
+            count = 0
+            dfs(i, set())
+            maxx = max(maxx, count)
         return maxx
             
 
