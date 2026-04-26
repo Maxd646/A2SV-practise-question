8ef18ea987b919e60b2878ee4fc8721992c431b5
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-04-26 11:58:36 +0300

    Time: 218 ms (43.24%) | Memory: 19.4 MB (48.65%) - LeetSync

--- 3815-sum-of-largest-prime-substrings/sum-of-largest-prime-substrings.py
+++ 3815-sum-of-largest-prime-substrings/sum-of-largest-prime-substrings.py
@@ -3,9 +3,8 @@ class Solution:
         def is_prime(num):
             if num < 2: return False
             if num == 2 or num == 3: return True
-            if num % 2 == 0 or num % 3 == 0: return False
-            for i in range(5, int(num**0.5) + 1, 6):
-                if num % i == 0 or num % (i + 2) == 0:
+            for i in range(2, int(num**0.5) + 1, 1):
+                if num % i == 0 :
                     return False
             return True
 
