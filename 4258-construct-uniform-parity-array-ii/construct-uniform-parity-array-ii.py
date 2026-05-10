0214ab1cb7e193cb8fb87046e18f4ca0f48db773
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-05-10 12:58:44 +0300

    Time: 17 ms (87.15%) | Memory: 35.5 MB (35.17%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,21 +1,12 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        odd = 0
-        seen = []
-        for num in nums1:
-            if num%2!=0:
-                odd+=1
-                seen.append(num)
-        if odd == len(nums1) or odd == 0:
+        if min(nums1)%2 == 1:
             return True
-        seen.sort()
-        # print(seen)
         for num in nums1:
-            if num%2==0:
-                x = bisect.bisect_left(seen, num)
-                if x==0:
-                    return False
+            if num%2==1:
+                return False
         return True
+        
             
             
         
