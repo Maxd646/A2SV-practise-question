9108d9a383ee714b2e40653fbaf7110d25eac3ed
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-05-11 11:27:10 +0300

    Add solution for Count Commas in Range II

---
+++ leetcode/3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        total  = 0
+        if n >= 10**3:
+            total += min(n, 10**6-1) -999
+        if n >= 10**6:
+            total += (min(n, 10**9 -1) - 10**6 +1)*2
+        if n >= 10**9:
+            total += (min(n, 10**12 -1)- 10**9 +1)*3
+        if n >= 10**12:
+            total += (min(n, 10**15 -1) - 10**12+ 1)*4
+        if n >= 10**15:
+            total +=5
+        return total
+
+        
