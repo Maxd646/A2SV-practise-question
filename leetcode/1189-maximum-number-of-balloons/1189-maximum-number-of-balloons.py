d65d785c0cfa881870f5444d82663c1252ddd20a
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-06-23 23:51:14 +0300

    Add solution for Maximum Number of Balloons

--- leetcode/1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ leetcode/1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -2,4 +2,5 @@ class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         a, b, l, n, o = text.count("a"), text.count("b"), (text.count("l"))//2, text.count("n"), (text.count("o"))//2
         return min([a, b, l, n, o])
+        # nnnn
         
