8236dfc5ace9c057ac1cf222bab46f20140e3fd8
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-06-24 23:56:42 +0300

    Add solution for Majority Element II

---
+++ leetcode/0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        seen= Counter(nums)
+        result=[]
+        for ch, num in seen.items():
+            if num>len(nums)/3:
+                result.append(ch)
+        return result
+        # nnn
+        
