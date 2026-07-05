7fafeba1d560188325d26fc3ff105a8b18d81c31
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-07-05 20:55:06 +0300

    Add solution for Distinct Prime Factors of Product of Array

---
+++ leetcode/2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def distinctPrimeFactors(self, nums: List[int]) -> int:
+        num = math.prod(nums)
+        # print(num)
+        ans = set()
+        i = 2
+        while num>1:
+            while num%i==0:
+                ans.add(i)
+                num//=i
+            i+=1
+        return len(ans)
+                
+            
+        
