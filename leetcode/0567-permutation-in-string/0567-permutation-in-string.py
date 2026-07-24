070d522dcfaa9b3ef3fc499b87f01578560bbba5
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-07-24 23:05:48 +0300

    Add solution for Permutation in String

---
+++ leetcode/0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n= len(s1)
+        m=len(s2)
+        if n>m:
+            return False
+        count=Counter(s1)
+        seen=Counter(s2[:n])
+        if count==seen:
+            return True
+        left=0
+        for i in range(n, m):
+            seen[s2[i]]+=1
+            seen[s2[left]]-=1
+            left+=1
+            if seen[s2[left]]==0:
+                del seen[s2[left]]
+            if seen==count:
+                return True
+        return False
+
+
+        
