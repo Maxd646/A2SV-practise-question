ed9b5518f3c89cb61bdb02089dce74daa5bba3aa
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-01 14:22:04 +0300

    Add solution for Is Subsequence

---
+++ leetcode/0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        left  = 0
+        for i in range(len(t)):
+            if s[left]==t[i]:
+                left+=1
+            if left == len(s):
+                return True
+        return False
+        
+        
