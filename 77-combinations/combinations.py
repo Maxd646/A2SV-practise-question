b5478238178be71f860ec3d196b54f9002474d06
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-17 17:50:40 +0300

    Time: 150 ms (15.99%) | Memory: 61.3 MB (53.43%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,23 +1,23 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
 
+        nums = [num for num in range(1, n+1)]
+
+
         def backtrack(i, combination):
             if len(combination) == k:
                 combinations.append(combination[:])
                 return
 
-            if i > n:
+            if i >= n:
                 return
+            
+            backtrack(i+1, combination + [nums[i]])
 
-           
-            combination.append(i)
             backtrack(i + 1, combination)
-            combination.pop()
 
-           
-            backtrack(i + 1, combination)
         combinations = []
-        backtrack(1, [])
+        backtrack(0, [])
         return combinations
 
 
