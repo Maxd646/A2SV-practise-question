e04f1cc1fffd7935d3a926d034e5861f8f69dfc9
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-17 22:09:45 +0300

    Add solution for Combination Sum II

--- leetcode/0040-combination-sum-ii/0040-combination-sum-ii.py
+++ leetcode/0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,19 +1,24 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+
+
+        ans = []
         candidates.sort()
-        ans= []
-        def backtrack(start, comb):
-            if sum(comb)==target:
-                ans.append(comb[:])
-            if sum(comb)>target:
+        def dfs(index, path, summ):
+
+            if summ == target:
+                ans.append(path[:])
                 return 
-            for i in range(start, len(candidates)):
-                if i > start and candidates[i] == candidates[i-1]:
-                    continue
-                comb.append(candidates[i])
-                backtrack(i+1, comb)
-                comb.pop()
-        backtrack(0, [])
-        return list(set([tuple(sorted(seen)) for seen in ans]))
         
+            if summ > target or index >= len(candidates):
+                return 
+            
+            dfs(index +1, path + [candidates[index]], summ + candidates[index])
+            while index + 1 < len(candidates) and candidates[index] == candidates[index + 1]:
+                index += 1
+
+            dfs (index +1, path, summ)
+        dfs(0, [], 0)
+
+        return list(set(tuple(sorted(num)) for num in ans))
         
