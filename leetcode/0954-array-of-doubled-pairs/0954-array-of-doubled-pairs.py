68748bcaf827f519c43ceceb659b17391f1ea91e
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-18 10:36:00 +0300

    Add solution for Array of Doubled Pairs

--- leetcode/0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.py
+++ leetcode/0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.py
@@ -1,27 +1,18 @@
 class Solution:
     def canReorderDoubled(self, arr: List[int]) -> bool:
 
-        arr.sort(key = abs)
+        arr.sort(key=abs)
         count = Counter(arr)
-        
-        for num in arr:
-            if count[num] ==0 :
-                continue 
-            if count[num*2] == 0:
-                return False
+        n = len(arr) // 2
+
+        x = 0
+        if arr.count(0)%2!= 0:
+            return False
             
-            count[num] -= 1
-            count[num*2] -= 1
+        for num in arr:
+            if count[num] > 0 and count[num * 2] > 0:
+                x += 1
+                count[num * 2] -= 1
+                count[num] -= 1
 
-        return True
-
-
-
-
-
-
-        
-
-
-
-        
+        return x == n
