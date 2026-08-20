0335b435d42f1bd55fd69b06be1cb8441d9fd34a
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-20 11:48:11 +0300

    Add solution for Removing Minimum and Maximum From Array

--- leetcode/2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ leetcode/2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -3,20 +3,13 @@ class Solution:
 
         minn, maxx = min(nums), max(nums)
         minn, maxx = nums.index(minn),nums.index(maxx) 
-        n = len(nums)-1
-        if minn == maxx:
-            return maxx+1
-        ans = float("inf")
-        print(minn, maxx)
-        if minn>maxx:
-            ans = min(ans, minn+1)
-            ans = min(ans, maxx+n-minn +2)
-            ans = min(ans, n-maxx+1)
-        if minn<maxx:
-            ans = min(ans, maxx+1)
-            ans = min(ans, minn+n-maxx+2)
-            ans = min(ans, n-minn+1)
-        return ans
+        n = len(nums)
+        x, y = min(minn, maxx), max(minn, maxx)
+        return min(y+1, n-x, 1+x+n-y)
+
+
+        
+       
     
     
         
