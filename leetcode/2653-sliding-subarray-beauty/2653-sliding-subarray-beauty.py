db6b56b3095d16d681f349547742e8ce23f07921
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-25 18:21:14 +0300

    Add solution for Sliding Subarray Beauty

--- leetcode/2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.py
+++ leetcode/2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.py
@@ -1,17 +1,37 @@
 class Solution:
     def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
 
-        arr = SortedList(nums[:k])
-        ans = []
-        ans.append(min(arr[x-1], 0))
+        def find():
 
-        for i in range(k, len(nums)):
+            count = 0
 
-            arr.remove(nums[i-k])
-            arr.add(nums[i])
-            ans.append(min(0, arr[x-1]))
+            for  i in range(102):
+
+                count += seen[i]
+
+                if count >= x:
+                    return min(0, i-50)
+
+            return 0
+
+        seen = [0]*102
+        ans  = []
+
+        for i in range(k):
+
+            seen[nums[i]+50] +=1
+
+        ans.append(find())
+
+        for j in range(k, len(nums)):
+
+            seen[nums[j] + 50] += 1
+            seen[nums[j-k]+50] -= 1
+            ans.append(find())
 
         return ans
+
+
 
        
 
