a8786af6f6c53efa2955d623cdd8d5087206cbbd
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-28 16:49:30 +0300

    Add solution for Unique Paths

--- leetcode/0062-unique-paths/0062-unique-paths.py
+++ leetcode/0062-unique-paths/0062-unique-paths.py
@@ -2,13 +2,18 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
         memo = {}
-        @lru_cache(None)
+       
         def dp(i, j):
             if i >= m or j >= n:
                 return 0
+
             if i == m -1 and j == n-1:
                 return 1
-            return dp(i, j+1) + dp(i+1, j)
+
+            if (i, j) not in memo:
+                memo[(i, j)] = dp(i, j+1) + dp(i+1, j)
+
+            return memo[(i, j)]
         return dp(0, 0)
 
         
