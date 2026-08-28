61c3b5ba36403c189db528563f333e48457b412c
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-28 22:47:04 +0300

    Add solution for Climbing Stairs

--- leetcode/0070-climbing-stairs/0070-climbing-stairs.py
+++ leetcode/0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
+        if n<=2:
+            return n
 
-        def dp(n):
-            if n == 2:
-                return 2
-            if n == 1:
-                return 1
-            if n not in memo:
-                memo[n] = dp(n-1) + dp(n-2)
-    
-            return memo[n]
-        return dp(n)
+        dp = [0]*(n+1)
+        
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n+1):
+
+            dp[i] = dp[i-1]+ dp[i-2]
+
+        return dp[n]
         
