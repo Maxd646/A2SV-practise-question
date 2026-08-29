fecba2e9684e9e348adad7dc760addef3e2461f4
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-29 22:32:31 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.18%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,16 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+
         n = len(nums)
         memo =  {}
+
         def maxval(i):
-            if i== 0:
+
+            if i== n-1:
                 return nums[i]
-            if i ==1:
-                return max(nums[1], nums[0])
+
+            if i == n-2:
+                return max(nums[n-1], nums[n-2])
+
             if i not in memo:
-                memo[i] = max(maxval(i-1), maxval(i-2)+nums[i])
+                memo[i] = max(maxval(i+1), maxval(i+2)+nums[i])
+
             return memo[i]
-        return maxval(n-1)
+
+        return maxval(0)
 
             
         
