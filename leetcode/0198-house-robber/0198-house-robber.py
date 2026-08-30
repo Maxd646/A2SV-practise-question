b9a66b223577a748d8328a8cc6c36375d7f6fbfa
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-30 16:06:36 +0300

    Add solution for House Robber

--- leetcode/0198-house-robber/0198-house-robber.py
+++ leetcode/0198-house-robber/0198-house-robber.py
@@ -1,16 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+
+
         n = len(nums)
-        memo =  {}
-        def maxval(i):
-            if i== 0:
-                return nums[i]
-            if i ==1:
-                return max(nums[1], nums[0])
-            if i not in memo:
-                memo[i] = max(maxval(i-1), maxval(i-2)+nums[i])
-            return memo[i]
-        return maxval(n-1)
+
+        if n <= 2:
+            return max(nums)
+
+        dp = [0]*n
+        dp[0] = nums[0]
+        dp[1] = max(nums[:2])
+        
+        for i in range(2, n):
+
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+
+        return dp[n-1]
+        
+
 
             
         
