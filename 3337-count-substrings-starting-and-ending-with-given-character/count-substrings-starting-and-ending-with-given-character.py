73c8269c3c2af89439847e64ec095b9d322fcb48
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-08-30 13:51:34 +0300

    Time: 18 ms (23.17%) | Memory: 20 MB (20.73%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.py
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.py
@@ -1,7 +1,13 @@
+
 class Solution:
     def countSubstrings(self, s: str, c: str) -> int:
+        count = 0
+        ans = 0
 
-        n = s.count(c)
-        return n*(n+1)//2
+        for ch in s:
+            if ch == c:
+                count += 1
+                ans += count
 
-         
+        return ans
+
