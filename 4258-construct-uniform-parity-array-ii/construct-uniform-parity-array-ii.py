404fe1b9b2b68cbf09c570bb87907d0d39a2c7d2
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-09-03 06:30:22 +0300

    Time: 11 ms (90.85%) | Memory: 35.5 MB (28.66%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,14 +1,10 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        if min(nums1)%2 == 1:
+        if min(nums1) % 2 == 1:
             return True
-        for num in nums1:
-            if num%2==1:
+        
+        for n in nums1:
+            if n % 2 == 1:
                 return False
         return True
-        
-            
-            
-        
-        
         
