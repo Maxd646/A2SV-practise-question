f35ffa7b4ffcef52a2b4ad071024f799d768900d
Daniel Kebede <ethiomiracle2017@gmail.com>
2026-09-12 08:38:53 +0300

    Time: 64 ms (88.98%) | Memory: 29.8 MB (66.93%) - LeetSync

---
+++ 2673-maximize-win-from-two-segments/maximize-win-from-two-segments.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximizeWin(self, prizePositions: List[int], k: int) -> int:
+
+        ans = 1
+        n = len(prizePositions)
+        com = [0]*(n+1)
+
+        count = 0
+        left = 0
+
+        for i in range(n):
+
+            while prizePositions[i] - prizePositions[left] > k:
+                left += 1
+
+            count = i - left + 1
+
+            ans = max(ans, count + com[left])
+
+            com[i+1] = max(count, com[i])
+
+       
+
+        return ans
+        
+        
